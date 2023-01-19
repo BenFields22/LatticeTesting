dc302e5db48fd95c20cec85286360207af188a55
EC2 Default User <ec2-user@ip-10-0-11-37.us-west-2.compute.internal>
2023-01-19 02:32:48 +0000

    Updated for Lattice config.

--- backend_1/index.js
+++ backend_1/index.js
@@ -4,7 +4,9 @@ var bodyParser = require('body-parser');
 const app = express();
 const port = 8080;
 const cors = require('cors');
-app.use(cors());
+app.use(cors({
+	origin: 'http://lattice-lb-639794587.us-west-2.elb.amazonaws.com'
+}));
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -16,7 +18,7 @@ app.get('/res1and2', async (req, res)  => {
 
     var msg = "Message from backend 2 is | "
     try {
-        const response = await axios.get('http://localhost:8083/msg2');
+        const response = await axios.get('http://backend-2-09f96317af092bcd5.7d67968.vpc-lattice-svcs.us-west-2.on.aws/msg2');
         console.log(response.data);
         msg = msg + response.data;
     } catch (error) {
